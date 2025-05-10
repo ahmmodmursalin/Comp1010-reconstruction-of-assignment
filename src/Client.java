f8f14e3c58122ae3fe8c8bb18aa7500b0389aec5
EXIA-GN-001 <de.guo@students.mq.edu.au>
2025-05-10 15:04:36 +1000

    feat: scaffold main flow

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,10 @@
 public class Client {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("rock johnson");
+        System.out.println("create staff Michael (Michael) Lay-Kujiraoka");
+        System.out.println("staff create a channel (Comp1010)");
+        System.out.println("Comp1010 holds two playlist Help session and Lectures");
+        System.out.println("in channel lecture create 12 videos week 1 to week 12");
+        System.out.println("Help Session will create 4 videos(session 1 to 4)");
     } 
 }
