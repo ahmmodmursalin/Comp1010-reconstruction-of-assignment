2e21843cfc6ffa0272d9174dd4dcf002aa7d8b53
EXIA-GN-001 <de.guo@students.mq.edu.au>
2025-05-17 16:28:45 +1000

    feat: add playlist and video class

--- src/Client.java
+++ src/Client.java
@@ -2,37 +2,56 @@ import java.util.ArrayList;
 
 public class Client {
     public static void main(String[] args) {
-System.out.println("==== Starting Staff and Channel System ====");
-System.out.println("\n-- Creating Staff Members --");
-System.out.println("Creating staff: Michael (Michael) Lay-Kujiraoka");
-Staff staff1 = new Staff("Spenser");
-System.out.println("Staff 1 created: " + staff1.getName());
-System.out.println("Staff 1 channels: " + (staff1.getChannels().isEmpty() ? "No channels assigned" : staff1.getChannels()));
+        System.out.println("==== Starting Staff and Channel System ====");
+        System.out.println("\n-- Creating Staff Members --");
+        System.out.println("Creating staff: Michael (Michael) Lay-Kujiraoka");
+        Staff staff1 = new Staff("Spenser");
+        System.out.println("Staff 1 created: " + staff1.getName());
+        System.out.println("Staff 1 channels: "
+                + (staff1.getChannels().isEmpty() ? "No channels assigned" : staff1.getChannels()));
 
-Staff staff2 = new Staff("Kevin");
-System.out.println("Staff 2 created: " + staff2.getName());
-System.out.println("Staff 2 channels: " + (staff2.getChannels().isEmpty() ? "No channels assigned" : staff2.getChannels()));
+        Staff staff2 = new Staff("Kevin");
+        System.out.println("Staff 2 created: " + staff2.getName());
+        System.out.println("Staff 2 channels: "
+                + (staff2.getChannels().isEmpty() ? "No channels assigned" : staff2.getChannels()));
 
-System.out.println("\n-- Creating Channels --");
-System.out.println("Creating a channel (Comp1010)...");
-Channel channel1 = new Channel("study helpers");
-System.out.println("Channel 1 created: " + channel1.getChannel());
+        System.out.println("\n-- Creating Channels --");
+        System.out.println("Creating a channel (Comp1010)...");
+        Channel channel1 = new Channel("study helpers");
+        System.out.println("Channel 1 created: " + channel1.getChannel());
 
-Channel channel2 = new Channel("lecture notes");
-System.out.println("Channel 2 created: " + channel2.getChannel());
+        Channel channel2 = new Channel("lecture notes");
+        System.out.println("Channel 2 created: " + channel2.getChannel());
 
-System.out.println("\n-- Assigning Channels to Staff --");
-staff1.createChannel(channel1);
-System.out.println("Added '" + channel1.getChannel() + "' channel to " + staff1.getName());
-System.out.println("Staff 1 channels: " + (staff1.getChannels()));
+        System.out.println("\n-- Assigning Channels to Staff --");
+        staff1.createChannel(channel1);
+        System.out.println("Added '" + channel1.getChannel() + "' channel to " + staff1.getName());
+        System.out.println("Staff 1 channels: " + (staff1.getChannels()));
 
-staff2.createChannel(channel2);
-System.out.println("Added '" + channel2.getChannel() + "' channel to " + staff2.getName());
-System.out.println("Staff 2 channels: " + (staff2.getChannels()));
+        staff2.createChannel(channel2);
+        System.out.println("Added '" + channel2.getChannel() + "' channel to " + staff2.getName());
+        System.out.println("Staff 2 channels: " + (staff2.getChannels()));
 
-System.out.println("\n-- Course Organization --");
-System.out.println("Comp1010 holds two playlists: 'Help Sessions' and 'Lectures'");
-System.out.println("In 'lecture notes' channel: Creating 12 videos (Week 1 to Week 12)");
+        System.out.println("\n-- Course Organization --");
+        System.out.println("Comp1010 holds two playlists: 'Help Sessions' and 'Lectures'");
+        Playlist playlist1 = new Playlist("Help Sessions");
+        System.out.println("playlist 1 " + playlist1.getVideos());
+        Playlist playlist2 = new Playlist("Lectures");
+        System.out.println("playlist 2 " + playlist2.getVideos());
+
+        playlist1.createVideo(new Video("how to study"));
+        playlist1.createVideo(new Video("basics of java"));
+        playlist1.createVideo(new Video("comp1000 recap"));
+        playlist1.createVideo(new Video("what to come in comp1010"));
+        System.out.println("playlist 1 videos" + playlist1.getVideos());
+
+        for (int i = 1; i <= 12; i++) {
+
+            playlist2.createVideo(new Video("week " + i));
+        }
+        System.out.println("playlist 2 videos" + playlist2.getVideos());
+
+        System.out.println("In 'lecture notes' channel: Creating 12 videos (Week 1 to Week 12)");
     }
 
 }
@@ -60,16 +79,67 @@ class Staff {
 }
 
 class Channel {
-    public String name;
+    private String name;
+    private ArrayList<Playlist> playlists;
 
     public Channel(String name) {
         this.name = name;
+        this.playlists = new ArrayList();
     }
 
     public String getChannel() {
         return this.name;
     }
-    public String toString(){
+
+    public String toString() {
+        return this.name;
+    }
+
+    public void createPlaylist(Playlist playlists) {
+        this.playlists.add(playlists);
+    }
+
+    public ArrayList<Playlist> getPlaylists() {
+        return this.playlists;
+    }
+
+}
+
+class Playlist {
+    private String name;
+    private ArrayList<Video> videos;
+
+    public Playlist(String name) {
+        this.name = name;
+        this.videos = new ArrayList();
+
+    }
+
+    public ArrayList<Video> getVideos() {
+        return this.videos;
+    }
+
+    public void createVideo(Video videos) {
+        this.videos.add(videos);
+    }
+
+    public void createMultipleVidoes(ArrayList<Video> videos) {
+        this.videos.addAll(videos);
+    }
+}
+
+class Video {
+    private String name;
+
+    public Video(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String toString() {
         return this.name;
     }
 }
