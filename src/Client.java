832477f6eba0ee79e408ce82a2b9afd22af32e51
EXIA-GN-001 <de.guo@students.mq.edu.au>
2025-05-27 22:48:30 +1000

    Feat: added Uml Diagram and Recursive Data Structure in a playlist

--- src/Client.java
+++ src/Client.java
@@ -4,12 +4,9 @@ public class Client {
     public static void main(String[] args) throws IOException {
         
         // starting the system
-        System.out.println("==== Starting Staff and Channel System ====");
 
         // creating staff members
-        System.out.println("\n-- Creating Staff Members --");
-        System.out.println("Creating staff: Michael (Michael) Lay-Kujiraoka");
-        Staff staff1 = new Staff("Spenser", "s1");
+        Staff staff1 = new Staff("Steven", "s1");
         System.out.println("Staff 1 created: " + staff1.getName());
         System.out.println("Staff 1 channels: " + 
             (staff1.getChannels().isEmpty() ? "No channels assigned" : staff1.getChannels()));
@@ -20,8 +17,6 @@ public class Client {
             (staff2.getChannels().isEmpty() ? "No channels assigned" : staff2.getChannels()));
 
         // making channels for videos
-        System.out.println("\n-- Creating Channels --");
-        System.out.println("Creating a channel (Comp1010)...");
         Channel channel1 = new Channel("study helpers");
         System.out.println("Channel 1 created: " + channel1.getChannel());
 
@@ -29,7 +24,6 @@ public class Client {
         System.out.println("Channel 2 created: " + channel2.getChannel());
 
         // assigning channels to each staff
-        System.out.println("\n-- Assigning Channels to Staff --");
         staff1.createChannel(channel1);
         System.out.println("Added '" + channel1.getChannel() + "' channel to " + staff1.getName());
         System.out.println("Staff 1 channels: " + staff1.getChannels());
@@ -39,46 +33,44 @@ public class Client {
         System.out.println("Staff 2 channels: " + staff2.getChannels());
 
         // organizing playlists
-        System.out.println("\n-- Course Organization --");
-        System.out.println("Comp1010 holds two playlists: 'Help Sessions' and 'Lectures'");
         Playlist playlist1 = new Playlist("Help Sessions");
         System.out.println("playlist 1 " + playlist1.getVideos());
         Playlist playlist2 = new Playlist("Lectures");
         System.out.println("playlist 2 " + playlist2.getVideos());
 
-        // adding some videos to help sessions
-        playlist1.createVideo(new Video("how to study"));
-        playlist1.createVideo(new Video("basics of java"));
-        playlist1.createVideo(new Video("comp1000 recap"));
-        playlist1.createVideo(new Video("what to come in comp1010"));
+        // adding some videos to Help Sessions Playlist
+        playlist1.createVideo(new Video("How to study"));
+        playlist1.createVideo(new Video("Basics of java"));
+        playlist1.createVideo(new Video("Comp1000 recap"));
+        playlist1.createVideo(new Video("What to come in comp1010"));
         System.out.println("playlist 1 videos" + playlist1.getVideos());
 
-        // adding 12 weekly lecture videos
+
+        // next video feature 
+        Video introPartOne = new Video("Intro to MqTube Part 1"); 
+        Video introPartTwo = new Video("intro to MqTube Part 2");
+        introPartOne.setNextVideo(introPartTwo);
+
+        // adding 12 weekly lecture videos to Lecture playlist
         for (int i = 1; i <= 12; i++) {
-            playlist2.createVideo(new Video("week " + i));
+            playlist2.createVideo(new Video("Week " + i));
         }
         System.out.println("playlist 2 videos" + playlist2.getVideos());
 
         System.out.println("In 'lecture notes' channel: Creating 12 videos (Week 1 to Week 12)");
 
         // creating course objects and linking staff
-        System.out.println("--- Course ---");
-        System.out.println("Create course 1");
-        System.out.println("Add staff channels");
-
-        Course course1 = new Course("comp1000");
-        System.err.println("course 1 " + course1.getName());
+        Course course1 = new Course("Comp1000");
+        System.err.println("Course 1 " + course1.getName());
         course1.addStaffs(staff1);
-        System.out.println("course 1 " + course1.getStaffs());
+        System.out.println("Course 1 " + course1.getStaffs());
 
-        Course course2 = new Course("comp1010");
-        System.err.println("course 2 " + course2.getName());
+        Course course2 = new Course("Comp1010");
+        System.err.println("Course 2 " + course2.getName());
         course2.addStaffs(staff2);
-        System.out.println("course 2 " + course2.getStaffs());
+        System.out.println("Course 2 " + course2.getStaffs());
 
         // saving channel info to a file
-        System.out.println("--- File IO ---");
-        System.out.println("Print all playlist from a course");
         course1.saveChannelsToCSV("educational video");
     }
 }
